7a3f66cfe426918ba8a872a68b59786715089cd5
Pedro Borges <pedro2205@gmail.com>
2019-09-25 21:55:28 -0300

    match matando aplicacao

--- api/controllers/LikeController.js
+++ api/controllers/LikeController.js
@@ -8,25 +8,39 @@
 module.exports = {
 
     like : (req,res) => {
-
+    
         sails.log.info("LikeController");
 
         const body = req.body.data;
         const user = body.user;
         const target = body.target;
         const sql = "insert into likes(user_id, like_id) values ( " + user.id + "," + target.id + " )";
+
+        
         
         sails.getDatastore("banco_dados").sendNativeQuery(sql,(err,resul) => {
            
             if( !err ){
                 sails.log.info(resul);
-                return res.json({ like : true });
+                
+                const sql = "select user_id,like_id from likes where user_id = target.id,like_id = user.id ";
+                sails.getDatastore("banco_dados").sendNativeQuery(sql,(err,resul) =>{
+                    if( !err ){
+                        const sql = "insert into match( user0_id, user1_id) values ( " + user.id + "," + target.id + " )";
+                        return res.json({match : true});
+                    }
+                    else{
+                        return res.json({match : false});
+                    }
+                });
+
+               return res.json({ like : true });
             }
             else {
                 sails.log.info(err);
                 return res.json({ like : false });
             }
-        });
+        }); 
     },
 
     superLike : (req,res) => {
--- api/controllers/MatchController.js
+++ api/controllers/MatchController.js
@@ -1,9 +1,13 @@
 
 module.exports = {
 
-    match : (req,res) => {
+ /*  match : (req,res) => {
+        const sql = "insert into match() values ()";
+    } */
 
-    }
+   /*  select : (req,res) => {
+        const sql = "select user1_id from match";
+    } */
 
 };
 
